77de0cb82ca1e59b020c3a01d6517af0bd2c385b
Marjorie0731 <62835103+Marjorie0731@users.noreply.github.com>
2020-05-27 13:21:40 +0800

    Update parse.py

--- src/data-preparation/parse.py
+++ src/data-preparation/parse.py
@@ -8,7 +8,7 @@ outfile = open('../../gen/data-preparation/temp/parsed-data.csv', 'w', encoding 
 
 # att_list = ['id', 'created_at', 'text', 'user.location']
 
-outfile.write('id\tcreated_at\ttext\tlocation\tRT_count\n')
+outfile.write('id\ttext\tlocation\n')
 
 cnt = 0
 nohashtag_cnt = 0
@@ -21,7 +21,9 @@ for line in con:
 
     text = obj.get('text')
 
-    text = text.replace('\t', '').replace('\n', '')
+    text = text.replace('\t', '')
+
+    text = text.replace('\n', '')
 
     user_location = obj.get('user').get('location')
 
@@ -29,7 +31,9 @@ for line in con:
         noloc_cnt += 1
         continue
 
-    cnt_retweet = obj.get('retweet_count')
+    user_location = user_location.replace('\n', '')
+    user_location = user_location.replace('\t', '')
+    user_location = user_location.replace('\"', '')
 
     # dataset info: num_total: 63856, num_withoutCorona: 3265, num_onlyContainWhiteHouse:
     # ['#coronavirus', '#DonaldTrump', '#Trump', '#PresidentTrump',
@@ -37,21 +41,28 @@ for line in con:
     # '#reopenamerica', and '#coronavirustaskforce']
 
     # filtering the tweet via hashtag
+    # ht_filter = ['PressBriefing', 'WhiteHouse', 'PressConference', 'Whitehousebriefing',
+    # 'reopenamerica', 'DonaldTrump']
 
-
-    ht_filter = ['Covid_19', 'covid19', 'Covid19', 'COVID19', 'Coronavirus', 'CoronaVirus', 'CORONAVIRUS', 'coronavirus', 'coronavirustaskforce', 'DonaldTrump', 'Trump', 'trump', 'TRUMP', 'PresidentTrump', 'PressBriefing',
-                 'WhiteHouse', 'PressConference', 'Whitehousebriefing', 'reopenamerica']
+    ht_filter = ['Covid_19', 'covid19', 'Covid19', 'COVID19', 'Coronavirus', 'CoronaVirus', 'CORONAVIRUS',
+                 'coronavirus', 'coronavirustaskforce', 'DonaldTrump', 'Trump', 'trump', 'TRUMP', 'PresidentTrump',
+                 'PressBriefing', 'WhiteHouse', 'PressConference', 'Whitehousebriefing', 'reopenamerica']
     obj_ht = obj.get('entities')['hashtags']
     filter_flag = False
     for h in obj_ht:
         if h['text'] in ht_filter:
             filter_flag = True
     if filter_flag:
-        outfile.write(obj.get('id_str')+'\t'+obj.get('created_at')+'\t'+text + '\t' + str(user_location) + '\t' + str(cnt_retweet) + '\n')
+        outfile.write(obj.get('id_str')+'\t' + text + '\t' + str(user_location) + '\n')
         cnt += 1
         print(str(cnt) + ' parsed')
     else:
         nohashtag_cnt += 1
+
+
+# 19989 tweets reserved
+# 23237 records have been cleaned because they lack location information
+# 20630 records have been cleaned because of incorrect hashtag
 
 print(str(noloc_cnt) + ' records have been cleaned because they lack location information')
 print(str(nohashtag_cnt) + ' records have been cleaned because of incorrect hashtag')
